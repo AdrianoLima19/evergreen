d4985ae03a158c85daee108dfa11c369893719c7
AdrianoLima19 <adrianolima.dev@gmail.com>
2024-10-30 17:18:47 -0300

    Adiciona animação de slide quando os elementos estão visíveis na tela com IntersectionObserver

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ window.addEventListener("submit", (e) => {
   e.preventDefault();
   e.target.reset();
 });
+
+const sliders = document.querySelectorAll(".slide-from-top, .slide-from-bottom, .slide-from-left, .slide-from-right");
+
+const slideOnView = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.target === sliders[0]) {
+        entry.target.classList.add("slide");
+      } else if (entry.intersectionRatio >= 0.7) {
+        entry.target.classList.add("slide");
+        slideOnView.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: [0, 0.7],
+  }
+);
+
+sliders.forEach((slider) => {
+  slideOnView.observe(slider);
+});
